00c1caa6789352cb7e7881738246e47dbcc48035
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2025-10-29 13:30:13 +0300

    fix indents in nvim

--- private_dot_config/nvim/lua/config/lazy-plugins.lua
+++ private_dot_config/nvim/lua/config/lazy-plugins.lua
@@ -1,74 +1,74 @@
 require("lazy").setup({
-	{ "NMAC427/guess-indent.nvim", opts = {} },
-	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
-		lazy = false,
-		build = ":TSUpdate",
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"mason-org/mason.nvim",
-				opts = {},
-			},
-			"mason-org/mason-lspconfig.nvim",
-			{
-				"WhoIsSethDaniel/mason-tool-installer.nvim",
-				opts = {
-					ensure_installed = { "stylua", "asm-lsp" },
-				},
-			},
-			{
-				"j-hui/fidget.nvim",
-				opts = {},
-			},
-			"saghen/blink.cmp",
-		},
-	},
-	{
-		"stevearc/conform.nvim",
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		opts = {
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-		},
-	},
-	{
-		"saghen/blink.cmp",
-		event = "VimEnter",
-		version = "1.*",
-		dependencies = {
-			"folke/lazydev.nvim",
-		},
-		opts = {
-			keymap = {
-				preset = "super-tab",
-			},
-			fuzzy = { implementation = "lua" },
-		},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
-		end,
-	},
+    { "NMAC427/guess-indent.nvim", opts = {} },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "master",
+        lazy = false,
+        build = ":TSUpdate",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "mason-org/mason.nvim",
+                opts = {},
+            },
+            "mason-org/mason-lspconfig.nvim",
+            {
+                "WhoIsSethDaniel/mason-tool-installer.nvim",
+                opts = {
+                    ensure_installed = { "stylua", "asm-lsp" },
+                },
+            },
+            {
+                "j-hui/fidget.nvim",
+                opts = {},
+            },
+            "saghen/blink.cmp",
+        },
+    },
+    {
+        "stevearc/conform.nvim",
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
+                end,
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+            },
+        },
+    },
+    {
+        "saghen/blink.cmp",
+        event = "VimEnter",
+        version = "1.*",
+        dependencies = {
+            "folke/lazydev.nvim",
+        },
+        opts = {
+            keymap = {
+                preset = "super-tab",
+            },
+            fuzzy = { implementation = "lua" },
+        },
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
+            vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Find existing buffers" })
+        end,
+    },
 })
 
 require("lspconfig").asm_lsp.setup({
-	filetypes = { "asm", "s", "S" },
+    filetypes = { "asm", "s", "S" },
 })
