1e5c7b719bb44e03606e82b6bcfe019f3e4db707
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2026-04-19 10:07:19 +0400

    Update .gitconfig Add .config/nvim/init.lua Remove .config/nvim/init.lua Change attributes of .config/nvim/nvim-pack-lock.json Change attributes of .config/nvim/lsp/lua_ls.lua Change attributes of .config/nvim/lsp/rust_analyzer.lua Change attributes of .config/nvim/lsp/ts_ls.lua Change attributes of .config/nvim/lua/config/lazy-plugins.lua Change attributes of .config/nvim/lua/config/lazy.lua Change attributes of .config/nvim/lua/keymap.lua Change attributes of .config/nvim/lua/lsp.lua Change attributes of .config/nvim/lua/plugins.lua Change attributes of .config/nvim/lua/plugins/betterTerm.lua Change attributes of .config/nvim/lua/plugins/fzf.lua

---
+++ private_dot_config/nvim/executable_init.lua
@@ -0,0 +1,65 @@
+vim.o.number = true
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.signcolumn = "yes"
+
+vim.o.updatetime = 250
+
+vim.o.timeoutlen = 2000
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.inccommand = "split"
+vim.o.confirm = true
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+
+if vim.fn.has("win32") == 1 then
+	vim.o.shell = "pwsh"
+else
+	vim.o.shell = "zsh"
+end
+-- clear highlights on search
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.diagnostic.config({
+	severity_sort = true,
+	float = { border = "rounded", source = "if_many" },
+	underline = { severity = vim.diagnostic.severity.ERROR },
+	signs = vim.g.have_nerd_font and {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	} or {},
+	virtual_text = {
+		source = "if_many",
+		spacing = 2,
+		format = function(diagnostic)
+			local diagnostic_message = {
+				[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				[vim.diagnostic.severity.WARN] = diagnostic.message,
+				[vim.diagnostic.severity.INFO] = diagnostic.message,
+				[vim.diagnostic.severity.HINT] = diagnostic.message,
+			}
+			return diagnostic_message[diagnostic.severity]
+		end,
+	},
+})
+
+require("plugins")
+require("keymap")
+require("lsp")
